f4db13483edab00dbbc99e735d82532b4d41cf38
linelie00 <dingpawn@gmail.com>
2024-11-20 16:49:11 +0900

    [Feat]: 코드 기본 세팅

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Page/Home.js';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* 위처럼 경로, 불러올 페이지 적어주시면 됩니다 */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/Page/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Styles/Home.css';
+
+const Home = () => {
+    return (
+        <div>
+
+            <h1 className='test'>Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
