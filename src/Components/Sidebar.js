7df6ba58ac9eaa5428bc4173b0a8fda373ab660b
linelie00 <san00@cu.ac.kr>
2024-12-29 04:33:36 +0900

    [Design]:사이드 바 디자인 수정 및 이미지 추가

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "../Styles/Components.css";
-
+import { ReactComponent as Icon1 } from "../Assets/images/btn/project_btn.svg";
+import { ReactComponent as Icon2 } from "../Assets/images/btn/inspection_btn.svg";
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="logo">쫑태통로</div>
       <nav>
         <ul>
-          <li>메뉴 1</li>
-          <li>메뉴 2</li>
+          <li>
+            <Icon1 className="menu-icon"/>
+            프로젝트
+          </li>
+          <li>
+            <Icon2 className="menu-icon"/>
+            이벤트 검수
+          </li>
           <li>메뉴 3</li>
         </ul>
       </nav>
