833616b80387edae713e32498b21bddac977e814
grovince <grovinceln@gmail.com>
2025-01-07 15:47:17 +0900

    [Design]: 로그인 UI 변경

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,14 +1,26 @@
+// Login.js
 import React from 'react';
-import LoginForm from '../Components/Auth/LoginForm';
+import LoginForm from '../Components/LoginForm';
 import '../Styles/Login.css'
+import {ReactComponent as Logo } from '../Assets/Imgs/etc/logo.svg';
 
 const Login = ({ login }) => {
     return (
         <div className="Login">
-            <div className="Login-title">
-                LOGIN
+            <div className="Login-wrapper">
+                <div className="Login-left">
+                    <div className="Login-image-container">
+                    <Logo className='Logo-icon' />
+                        <span className='Logo-text'>쫑태통로</span>
+                    </div>
+                </div>
+                <div className="Login-right">
+                    <div className="Login-header">
+                        <span className="login-text">관리자 로그인</span>
+                    </div>
+                    <LoginForm login={login} />
+                </div>
             </div>
-            <LoginForm login={login} />
         </div>
     );
 };
