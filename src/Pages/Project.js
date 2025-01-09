38084fbbcac444aca820a5edd35a4b4022e718f9
linelie00 <san00@cu.ac.kr>
2025-01-10 03:12:01 +0900

    [Feat]:프로젝트 단계 디자인

--- src/Pages/Project.js
+++ src/Pages/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../Styles/Home.css';
+import '../Styles/Project.css';
 import NameTag from '../Components/NameTag';
 import Title from '../Components/Title';
 
@@ -11,6 +12,23 @@ const Project = () => {
             title="프로젝트"
             desc="프로젝트 관리, 업로드 및 분석"
             />
+            <div className="step-container">
+                <div className="num active">1</div>
+                <p>선택</p>
+                <span>진행중</span>
+                <hr class="hr-basic" />
+                <div className="num">2</div>
+                <p>업로드</p>
+                <span>대기중</span>
+                <hr class="hr-basic" />
+                <div className="num">3</div>
+                <p>분석</p>
+                <span>대기중</span>
+                <hr class="hr-basic" />
+                <div className="num">4</div>
+                <p>완료</p>
+                <span>대기중</span>
+            </div>
             <h1>project</h1>
         </div>
     );
