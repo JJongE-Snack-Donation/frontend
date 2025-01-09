79d9533b4af9590fe34d641ec69683b64ca22465
grovince <grovinceln@gmail.com>
2025-01-09 16:33:48 +0900

    [Feat]: 이미지 리스트 작업 중

--- src/Components/Pagination/ImageGrid.js
+++ src/Components/Pagination/ImageGrid.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import '../../Styles/Pagination/ImageGrid.css';
-//import { testImages } from '../../Data/testImages';
+import trash_bin from '../../Assets/Imgs/btn/trash_bin.svg'
 
-const ImageGrid = ({ images }) => {
+const ImageGrid = ({ images, onImageClick }) => {
   return (
     <div className="image-grid">
       {images.map((image) => (
-        <div key={`${image.FileName}`} className="image-item">
-          <img src={image.FilePath} alt={image.FileName} />
+        <div key={image.id} className="image-card" onClick={() => onImageClick(image)}>
+          <div className="image-header">
+            <input type='checkbox'/>
+            <p className="image-title">{image.FileName}</p>
+            <button className='image-delete'><img src={trash_bin} alt="trash_bin"/></button>
+          </div>
+          <div className="image-wrapper">
+            <img src={image.FilePath} alt="" />
+          </div>
+          <div className="image-info">
+            <p>종명 {image.species || '물체 미감지'}</p>
+            <p>최대 개체수 {image.maxCount || '0'}</p>
+            <p>카메라 라벨 {image.UserLabel}</p>
+            <p>촬영 날짜 {image.DateTimeOriginal}</p>
+          </div>
         </div>
       ))}
     </div>
