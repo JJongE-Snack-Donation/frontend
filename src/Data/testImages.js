4d6c526508d540aed61bf0e3086f106bd34c3748
grovince <grovinceln@gmail.com>
2025-01-25 16:42:15 +0900

    [Fix]: 테스트 이미지 구성 수정

--- src/Data/testImages.js
+++ src/Data/testImages.js
@@ -1,18 +1,18 @@
 export const testImages = [
-    { projectName: '테스트1', imageId: '1', FilePath: '/images/test/cat1.png', FileName: 'Image 1', DateTimeOriginal: '2020-04-28T20:43:45.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon'},
-    { projectName: '테스트1', imageId: '2', FilePath: '/images/test/cat2.png', FileName: 'Image 2', DateTimeOriginal: '2020-04-28T20:43:46.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '3', FilePath: '/images/test/cat3.png', FileName: 'Image 3', DateTimeOriginal: '2020-04-28T20:44:06.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '4', FilePath: '/images/test/cat4.png', FileName: 'Image 4', DateTimeOriginal: '2020-04-28T20:44:07.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '5', FilePath: '/images/test/cat5.png', FileName: 'Image 5', DateTimeOriginal: '2020-04-28T20:44:08.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '6', FilePath: '/images/test/cat6.png', FileName: 'Image 6', DateTimeOriginal: '2020-04-28T20:44:26.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '7', FilePath: '/images/test/cat7.png', FileName: 'Image 7', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
-    { projectName: '테스트1', imageId: '8', FilePath: '/images/test/cat8.png', FileName: 'Image 8', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'wildboar' },
-    { projectName: '테스트1', imageId: '9', FilePath: '/images/test/cat9.png', FileName: 'Image 9', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'wildboar' },
-    { projectName: '테스트1', imageId: '10', FilePath: '/images/test/cat10.png', FileName: 'Image 10', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
-    { projectName: '테스트1', imageId: '11', FilePath: '/images/test/cat11.png', FileName: 'Image 11', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
-    { projectName: '테스트1', imageId: '12', FilePath: '/images/test/cat12.png', FileName: 'Image 12', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
-    { projectName: '테스트1', imageId: '13', FilePath: '/images/test/cat13.png', FileName: 'Image 13', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
-    { projectName: '테스트1', imageId: '14', FilePath: '/images/test/cat14.png', FileName: 'Image 14', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' }
+    { projectName: '테스트1', imageId: '1', FilePath: '/images/test/cat1.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:43:45.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon'},
+    { projectName: '테스트1', imageId: '2', FilePath: '/images/test/cat2.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:43:46.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '3', FilePath: '/images/test/cat3.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:06.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '4', FilePath: '/images/test/cat4.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:07.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '5', FilePath: '/images/test/cat5.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:08.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '6', FilePath: '/images/test/cat6.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:26.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '7', FilePath: '/images/test/cat7.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'raccoon' },
+    { projectName: '테스트1', imageId: '8', FilePath: '/images/test/cat8.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'wildboar' },
+    { projectName: '테스트1', imageId: '9', FilePath: '/images/test/cat9.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:27.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'wildboar' },
+    { projectName: '테스트1', imageId: '10', FilePath: '/images/test/cat10.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
+    { projectName: '테스트1', imageId: '11', FilePath: '/images/test/cat11.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
+    { projectName: '테스트1', imageId: '12', FilePath: '/images/test/cat12.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
+    { projectName: '테스트1', imageId: '13', FilePath: '/images/test/cat13.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' },
+    { projectName: '테스트1', imageId: '14', FilePath: '/images/test/cat14.png', FileName: 'test1-images', DateTimeOriginal: '2020-04-28T20:44:28.000Z', SerialNumber: 'H600HH10220605', UserLabel: 'HC600 HYPERFIRE', species: 'waterdeer' }
   ];
   
   export default testImages;
