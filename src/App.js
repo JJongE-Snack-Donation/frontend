9f54d83610bcf79da82e936563971576d3c76e53
linelie00 <san00@cu.ac.kr>
2025-02-08 23:20:14 +0900

    [Feat]: 로그인 미완료 시 로그인 이외 페이지 진입 불가 및 페이지 이동

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SidebarLayout from "./Layouts/SidebarLayout";
 import AuthLayout from "./Layouts/AuthLayout";
-import Home from './Pages/Home.js';
 import Project from './Pages/Project.js';
 import Camera from './Pages/Cam.js';
 import ImageInquiry from './Pages/ImageInquiry.js';
@@ -10,27 +9,33 @@ import GeneralInspection from './Pages/GInspection.js';
 import ExceptionInspection from './Pages/ExInspection.js';
 import Login from './Pages/Login.js';
 import './Assets/Font/Font.css';
+import ProtectedRoute from "./Components/ProtectedRoute";  // 경로에 맞게 수정
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Routes>
-            {/* 기본 레이아웃 (사이드바 포함) */}
-            <Route element={<SidebarLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/project" element={<Project />} />
-              <Route path="/camera" element={<Camera />} />
-              <Route path="/image-inquiry" element={<ImageInquiry />} />
-              <Route path="/general-inspection" element={<GeneralInspection />} />
-              <Route path="/exception-inspection" element={<ExceptionInspection />} />
-            </Route>
+        <Routes>
+          {/* 보호된 라우트 */}
+          <Route
+            element={
+              <ProtectedRoute>
+                <SidebarLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/project" element={<Project />} />
+            <Route path="/camera" element={<Camera />} />
+            <Route path="/image-inquiry" element={<ImageInquiry />} />
+            <Route path="/general-inspection" element={<GeneralInspection />} />
+            <Route path="/exception-inspection" element={<ExceptionInspection />} />
+          </Route>
 
-            {/* 인증 레이아웃 (사이드바 미포함) */}
-            <Route element={<AuthLayout />}>
-              <Route path="/login" element={<Login/>} />
-            </Route>
-          </Routes>
+          {/* 인증 레이아웃 */}
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
       </Router>
     </div>
   );
