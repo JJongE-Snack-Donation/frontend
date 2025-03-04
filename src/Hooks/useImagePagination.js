2ec620c1eb166c63e84d0fef82d00e1b9e1a5d3a
grovince <grovinceln@gmail.com>
2025-03-04 19:14:53 +0900

    [Remove]: 주석 코드 삭제

--- src/Hooks/useImagePagination.js
+++ src/Hooks/useImagePagination.js
@@ -9,44 +9,6 @@ const useImagePagination = (itemsPerPage = 12) => {
   const { testImageData } = useSearch();
   const [totalItems, setTotalItems] = useState(testImageData.length);
 
-  //백엔드 연결 시 수정 
-/* 
-    useEffect(() => {
-      fetchImages(currentPage);
-    }, [currentPage]);
-
-    const fetchImages = async (page) => {
-      try {
-        const response = await axios.get('/api/images', {
-          params: {
-            page: page,
-            per_page: itemsPerPage,
-            projectName: projectName || 'all',
-            date: date || 'all',
-            cameraSerial: cameraSerial || 'all',
-            cameraLabel: cameraLabel || 'all',
-            species: species || 'all'
-          }
-        });
-        
-        if (response.data.images) {
-          setImages(response.data.images);
-          setTotalItems(response.data.total);
-        }
-      } catch (error) {
-        console.error('이미지 불러오기 실패:', error);
-        // 에러 발생 시 빈 배열로 초기화
-        setImages([]);
-        setTotalItems(0);
-      }
-    };
-
-    const handlePageChange = (pageNumber) => {
-      setCurrentPage(pageNumber);
-      fetchImages(pageNumber);
-    };
-
-*/
 
   //페이지네이션 기능 테스트 코드
   useEffect(() => {
